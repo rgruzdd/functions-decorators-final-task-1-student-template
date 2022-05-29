99c3d5c5057b03fffe8619c7131b76d3ee1a88db
Дмитрий Колб <dm.kolb@gmail.com>
2022-05-29 15:23:58 +0000

    Update tasks/task.py

--- tasks/task.py
+++ tasks/task.py
@@ -1,6 +1,6 @@
 from typing import List
 
-def split(data: str, sep=None, maxsplit=- 1):
+def split(data: str, sep=None, maxsplit=-1):
     """
     Add your code here or call it from here   
     """
