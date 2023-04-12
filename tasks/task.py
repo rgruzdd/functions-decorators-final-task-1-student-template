a14b731355482c81dfe593343ed070dbd113d9e7
Gruzin <gruzd.roman@inbox.ru>
2023-04-13 00:02:57 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -3,19 +3,22 @@ def split(data: str, sep=None, maxsplit=-1):
         sep = ' '
     res = []
     num_of_splits = 0
+    if data == '':
+        res.append(data)
+        return res
     while data != '':
         if data.find(sep) != -1 and ( num_of_splits < maxsplit or maxsplit == -1 ):
             new_word = data[0:data.find(sep)]
             if new_word == '':
                 data = data.replace(sep, '', 1)
-                res.append(new_word)
-                num_of_splits += 1
                 continue
             res.append(new_word)
-            data = data.replace(data[0:data.find(sep)]+sep, '', 1)
+            data = data.replace(data[0:data.find(sep)], '', 1)
             num_of_splits += 1
         else:
+            if data.find(sep) != -1:
+                data = data.replace(sep, '', 1)
             res.append(data)
             break
-    return res
 
+    return res
