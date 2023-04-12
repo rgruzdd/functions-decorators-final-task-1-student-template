1d92679202e54a8f0d55eeecaa44cf28159cac11
Gruzin <gruzd.roman@inbox.ru>
2023-04-12 15:16:24 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -1,7 +1,16 @@
 from typing import List
 
 def split(data: str, sep=None, maxsplit=-1):
-    """
-    Add your code here or call it from here   
-    """
-    pass
+    if sep == None:
+        sep = ' '
+    res = []
+    new_word = ''
+    for i in data:
+        if i == sep:
+            res.append(new_word)
+            new_word = ''
+        else:
+            new_word += i
+    res.append(new_word)
+    return res
+
