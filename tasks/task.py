9daf0e3e2fe67e5d2d2ccf76482da19fb8fa945e
Gruzin <gruzd.roman@inbox.ru>
2023-04-12 22:51:07 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -1,14 +1,48 @@
 from typing import List
 
+# def split(data: str, sep=None, maxsplit=-1):
+#     res = []
+#     new_word = ''
+#     for i in data:
+#         if i != ' ':
+#             new_word += i
+#         else:
+#             i = ' '
+#             res.append(new_word)
+#             new_word = ''
+#     res.append(new_word)
+#     return res
+
+
+# from typing import List
+#
 def split(data: str, sep=None, maxsplit=-1):
     res = []
+    new_data = data
+    if sep is not None:
+        new_data = data.replace(sep, ':')
+    else:
+        sep = ' '
+    num_of_splits = 0
     new_word = ''
-    for i in data:
-        if i != ' ':
+    for i in new_data:
+        if i != ':':
             new_word += i
         else:
-            i = ' '
-            res.append(new_word)
-            new_word = ''
-    res.append(new_word)
+            if maxsplit == -1:
+                if new_word != '':
+                    res.append(new_word)
+                new_word = ''
+            elif num_of_splits < maxsplit:
+                if new_word != '':
+                    res.append(new_word)
+                new_word = ''
+                num_of_splits += 1
+            else:
+                new_word += i
+
+    if new_word is not None:
+        res.append(new_word)
     return res
+
+
